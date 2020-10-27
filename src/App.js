d8238142cab8677e3286315e539d1b6883e8dd59
hdmcgehee <hdmcgehee@gmail.com>
2020-10-27 15:50:54 -0400

    Almost at mvp, questions still repeat

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from './Components/Modal';
 import QuestionData from './Data/Apprentice_TandemFor400_Data.json';
 
 import './App.css';
 
 export default function App() {
+  const QuestionArry = [];
+  
+  for(let i = 0; i < QuestionData.length; i++){
+    QuestionArry.push(QuestionData[i]);
+  }
+  // state info
+  
   const [showModal, setModal] = useState(false);
-
+  
   let [score, setScore] = useState(0);
-
+  
   let [currentQuestion, setCurrentQuestion] = useState(0);
-
+  
   let [questionContent, setQuestionContent] = useState(Math.floor(Math.random() * 21));
-
+  
   const [showScore, showEndScore] = useState(false);
+  
+  //functions
+  
+  const LoadData = () => {
+  }
 
   const Answer = () => {
-
+    QuestionArry.splice(QuestionArry[questionContent - 1], 1);
+    console.log(QuestionArry[questionContent - 1]);
+    
     let nextQuestion = currentQuestion + 1;
     if(nextQuestion < 10){
       setModal(true);
@@ -35,7 +49,7 @@ export default function App() {
   
   const CloseModal = () => {
     let nextQuestion = currentQuestion + 1;
-
+    
     if(nextQuestion < 10){
       setCurrentQuestion(nextQuestion);
       setQuestionContent(Math.floor(Math.random() * 21))
@@ -44,17 +58,31 @@ export default function App() {
     setModal(false);
   }
 
+  const Reset = () => {
+    showEndScore(false)
+    setCurrentQuestion(0)
+    setScore(0)
+  }
+
+
   return (
     <div className="App">
       <div className="header">
         <h1>Trivia</h1>
       </div>
 
-      <Modal show={showModal} close={CloseModal} correct={QuestionData[questionContent].correct}/>
+      
+
+      <Modal show={showModal} close={CloseModal} correct={QuestionArry[questionContent].correct}/>
 
       <div className="questionContainer">
         {showScore ? (
-          <div>You scored {score} out of 10</div>
+          <div>
+            <h1>You scored {score} out of 10</h1>
+            <button className="resetButton" onClick={Reset}>
+                Play Again?
+            </button>
+          </div>
         ) : (
           <>
             <div className="question-section">
@@ -62,13 +90,13 @@ export default function App() {
                   <span>Question {currentQuestion + 1}</span>/10
                 </div>
                 <div className="question-text">
-                  {QuestionData[questionContent].question}
+                  {QuestionArry[questionContent].question}
                 </div>
             </div>
 
             <div className="answer-section">
                 
-              {QuestionData[questionContent].incorrect.map((option) => (
+              {QuestionArry[questionContent].incorrect.map((option) => (
                 <button className="regButton" onClick={Answer}>
                     {option}
                 </button>
@@ -78,7 +106,7 @@ export default function App() {
                 Correct();
                 Answer();
               }}>
-                {QuestionData[questionContent].correct}
+                {QuestionArry[questionContent].correct}
               </button>
 
             </div>
